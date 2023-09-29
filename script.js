1242f77c42b7416c26ce1375e660055d7a6aca50
Niroj Neupane Khatri <knirojneupane@gmail.com>
2023-09-29 18:28:41 +0545

    Fix issue with floating point operations

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const numbers = document.querySelector('#numbers');
-const operators = document.querySelector('#operators');
+const numberkeys = document.querySelectorAll('.numbers');
+const operatorkeys = document.querySelectorAll('.operators');
 
-const numberkeys = numbers.querySelectorAll('.key');
-const operatorkeys = operators.querySelectorAll('.key');
+let operatorCount = 0;
+let operator = "";
 
 const screen = document.getElementById('screen');
 numberkeys.forEach((key) => {
@@ -21,5 +21,46 @@ operatorkeys.forEach((key) => {
             console.log(str);
             screen.textContent = str;
         }
+        else if(key.id==='equals'){
+            //do the thing for equals
+            result = operate(screen.textContent,operator);
+            result = (Math.round(result * 100) / 100);
+            screen.textContent = result;
+            operatorCount=0;
+        }
+        else{
+            if(operatorCount == 0){
+                operatorCount++;
+                operator = key.textContent;
+                screen.textContent = screen.textContent + key.textContent;
+            }
+            else if(operatorCount ==1){
+                //do the thing for equals
+                result = operate(screen.textContent,operator);
+                result = (Math.round(result * 100) / 100);
+                operator= key.textContent;
+                screen.textContent = result+key.textContent;
+            }
+        }
     })
 });
+
+function operate(string,op){
+    const operands = string.split(op);
+    console.log(operands+op);
+    if(op=='+'){
+        return Number(operands[0]) + Number(operands[1]);
+    }
+    if(op=='-'){
+        return operands[0] - operands[1];
+    }
+    if(op=='*'){
+        return operands[0] * operands[1];
+    }
+    if(op=='/'){
+        return operands[0] / operands[1];
+    }
+    if(op=='%'){
+        return operands[0] % operands[1];
+    }
+}
